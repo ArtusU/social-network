ea3958b94d8ad6b240944e50a96cc2fe5289d617
ArtusU <artusrock@hotmail.com>
2020-11-01 09:43:33 +0000

    base.html navbar.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request):
+    user = request.user
+    hello = 'Hello world'
+
+    context = {
+        'user': user,
+        'hello' : hello,
+    }
+    return render(request, 'home.html', context)
+    # return HttpResponse('Hello world')
