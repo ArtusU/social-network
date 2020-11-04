1b5690c9b42171e63c529affea4426cd9179055f
ArtusU <artusrock@hotmail.com>
2020-11-04 09:46:30 +0000

    profile update

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from profiles.models import Profile
+from .forms import ProfileModelForm
 
 def home_view(request):
     user = request.user
@@ -17,9 +18,18 @@ def home_view(request):
 
 def my_profile_view(request):
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            confirm = True
 
     context = {
-        'profile': profile
+        'profile': profile,
+        'form': form,
+        'confirm': confirm
     }
     
     return render(request, 'profiles/myprofile.html', context)
